84ec61b92ba209d8ac28b8bf606d661d510834f9
Mihir123456JJ <kalothiamihir@gmail.com>
2024-09-30 18:24:23 +0530

    List in Python

---
+++ main.py
@@ -0,0 +1,13 @@
+#Create an empty list
+x=[]
+print(x)
+#A list with elements
+z=["Mihir",77,True,56.90]
+print(z)
+#Use operator
+c=[1,2,"play"]*9
+print(c)
+#Reverse a list
+y=[1,2,3,4,5]
+r=y[::-1]
+print(r)
